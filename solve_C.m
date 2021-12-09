ad6b0dfc2759480f880ff0fbb7c7bf4be00609d3
Fancrey <fancrey@gmail.com>
2021-12-09 17:06:46 +0800

    init

--- solve_C.m
+++ solve_C.m
@@ -2,34 +2,43 @@ function [C,Newdata, Newgnd] = solve_C(data, label, testRatio, nClass)
 % testRatio: Proportion of test data in total data
 % label: the label of all data
 % data: m*n m:Feature dimension. n: Number of data
-% 训练集索引
+%?
 testRatio=1-testRatio;
 n=size(data, 2);
 m=size(data, 1);
 if testRatio~=1
-trainIndices = crossvalind('HoldOut', n, testRatio);
-%trainIndices = logical(floor(rand(n,1)*(1/testRatio)));
-% 测试集索引
-testIndices = ~trainIndices;
-
-% 训练集和训练标签
-trainData = data(:,trainIndices);
-trainLabel = label(trainIndices,:);
-k=size(trainData,2);
-length(unique(trainLabel))
-% 测试集和测试标签
-testData = data(:,testIndices);
-testLabel= label(testIndices, :);
-
-
-Newdata=[trainData testData];
-Newgnd=[trainLabel;testLabel];
-C = zeros(n,n+nClass-k);
-Q=zeros(k,nClass);
-for i=1:k
-Q(i,trainLabel(i))=1;
-end
-C=[Q zeros(k,n-k);zeros(n-k,nClass) eye(n-k)];
+    trainIndices = crossvalind('HoldOut', n, testRatio);
+    trainLabel = label(trainIndices,:);
+    if length(unique(trainLabel))<length(unique(label))
+        qs=find(ismember(unique(label),unique(trainLabel))==0);
+        if length(qs)==1
+            candid=find(qs==label);
+            trainIndices(candid(1))=1;
+        else
+            qs_num=length(qs);
+            for index=1:qs_num
+                candid=find(qs(index)==label);
+                trainIndices(candid(1))=1;
+            end
+        end
+    end
+    trainLabel = label(trainIndices,:);
+    testIndices = ~trainIndices;
+    trainData = data(:,trainIndices);
+    k=size(trainData,2);
+    length(unique(trainLabel))
+    testData = data(:,testIndices);
+    testLabel= label(testIndices, :);
+    
+    
+    Newdata=[trainData testData];
+    Newgnd=[trainLabel;testLabel];
+    C = zeros(n,n+nClass-k);
+    Q=zeros(k,nClass);
+    for i=1:k
+        Q(i,trainLabel(i))=1;
+    end
+    C=[Q zeros(k,n-k);zeros(n-k,nClass) eye(n-k)];
 else
     k=0
     C = zeros(n,n+nClass-k);
@@ -38,4 +47,4 @@ else
     Newdata=data;
     Newgnd=label;
 end
-    
+
